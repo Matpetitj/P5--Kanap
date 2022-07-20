35329800ab1f8ce0c93f95007dd794e739bad2b0
Mathis Petitjean <veille.petitjean@gmail.com>
2022-07-20 10:53:55 +0200

    Début du refactoring product & cart

--- front/js/product.js
+++ front/js/product.js
@@ -12,16 +12,12 @@ fetch("http://localhost:3000/api/products")
 .then((res) => res.json())
   .then((listSofa) => {
     console.table(listSofa);
-    pageProducts(listSofa);
+    insertProductIntoPage(listSofa);
   })
-
-  let productAddToCart = {};
-
-  productAddToCart._id = productId;
 
   //Ajout des informations des produits
 
-  function pageProducts(products) {
+  function insertProductIntoPage(products) {
     const zoneImage = document.querySelector("article div.item__img");
     const productTitle = document.getElementById("title");
     const productPrice = document.getElementById("price");
@@ -44,8 +40,6 @@ fetch("http://localhost:3000/api/products")
 
             productDescription.textContent = choice.description;
 
-            productAddToCart.productPrice = choice.price;
-
             for (const colors of choice.colors) {
                 productColor.innerHTML += `<option value="${colors}">${colors}</option>`; //utilisation littéraux de gabarits
             }
@@ -56,39 +50,26 @@ fetch("http://localhost:3000/api/products")
 
   //Ajout du produit au panier
 
-let colorChoice = document.getElementById("colors");
-
-  colorChoice.addEventListener("input", (ec) => {
-
-  let productColor;
-  productColor = ec.target.value;
-  productAddToCart.color = productColor;
-  document.getElementById("addToCart").style.color = "white";
-  document.getElementById("addToCart").textContent = "Ajouter au panier";
-  console.log(productColor);
-});
-
-let quantityChoice = document.querySelector('input[id="quantity"]');
-let productQuantity;
-// On écoute ce qu'il se passe dans input[name="itemQuantity"]
-quantityChoice.addEventListener("input", (eq) => {
-  productQuantity = eq.target.value;
-  productAddToCart.quantity = productQuantity;
-  //reset
-  document.getElementById("addToCart").style.color = "white";
-  document.getElementById("addToCart").textContent = "Ajouter au panier";
-  console.log(productQuantity);
-});
+function addToCart(productId, color, amount) {
+  console.log(productId, color, amount);
+  //récupérer le panier
+  //vérifier si le produit existe déjà avec l'id et la couleur identiques
+  //si il existe, + les deux quantités (avant maintenant)
+  //sinon, insérer le produit dans le panier
+  //sauvegarder le panier dans le localStorage
+}
 
 let productChoice = document.getElementById("addToCart");
 productChoice.addEventListener("click", () => {
+  let colorChoice = document.getElementById("colors");
+  let quantityChoice = document.querySelector('input[id="quantity"]');
   if (
    //valeurs dynamiques (non définies au départ, il faut cliquer et changer la valeur pour accéder à la logique)
-    productAddToCart.quantity < 1 ||
-    productAddToCart.quantity > 100 ||
-    productAddToCart.quantity === undefined ||
-    productAddToCart.color === "" ||
-    productAddToCart.color === undefined
+    quantityChoice.value.quantity < 1 ||
+    quantityChoice.value.quantity > 100 ||
+    quantityChoice.value.quantity === undefined ||
+    colorChoice.value.color === "" ||
+    colorChoice.value.color === undefined
   ) {
     alert("Pour valider votre choix, veuillez renseigner une couleur et/ou une quantité valide");
   } else {
@@ -96,58 +77,82 @@ productChoice.addEventListener("click", () => {
     console.log("clic effectué");
     document.getElementById("addToCart").textContent = "Et Hop!";
   }
+
+  addToCart(productId,colorChoice.value, quantityChoice.value);
 });
-// déclaration tableau qui sera le 1er, unique et destiné à initialiser le panier
-let clientProductChoice = [];
-let productSave = [];
-let temporaryProduct = [];
-let pushingProduct = [];
 
-function addFirstProduct() {
-  console.log(productSave);
-  //if productSave est null, existe pas/ pas créé
-  if (productSave === null) {
-    // push le produit choisit
-    clientProductChoice.push(productAddToCart);
-    console.log(productAddToCart);
-    return (localStorage.cartStock = JSON.stringify(clientProductChoice));
-  }
-}
 
-function addOtherProduct() {
-  // vide/initialise pushing... pour recevoir les nouvelles données
-  pushingProduct = [];
-  // push le produit choisit dans temporary
-  temporaryProduct.push(productAddToCart);
-  pushingProduct = productSave.concat(temporaryProduct);
-  //Triage avec technologie "sort"
-  pushingProduct.sort(function sorting(a, b) {
-    if (a._id < b._id) return -1;
-    if (a._id > b._id) return 1;
-    if (a._id = b._id){
-      if (a.color < b.color) return -1;
-      if (a.color > b.color) return 1;
-    }
-    return 0;
-  });
-  temporaryProduct = [];
-  return (localStorage.cartStock = JSON.stringify(pushingProduct));
-}
 
-function Cart() {
-  productSave = JSON.parse(localStorage.getItem("cartStock"));
-  if (productSave) {
-    for (let choice of productSave) {
-      //comparateur articles choisis et déjà ajoutés
-      if (choice._id === productId && choice.color === productAddToCart.color) {
-        alert("RAPPEL: Ce produit existe déjà dans votre panier");
-        let addQuantity = parseInt(choice.quantity) + parseInt(productQuantity);
-        choice.quantity = JSON.stringify(addQuantity);
-        // dernière commande, on renvoit un nouveau panierStocké dans le localStorage
-        return (localStorage.cartStock = JSON.stringify(productSave));
-      }
-    }
-    return addOtherProduct();
-  }
-  return addFirstProduct();
-}
+// let colorChoice = document.getElementById("colors");
+
+//   colorChoice.addEventListener("input", (ec) => {
+
+//   document.getElementById("addToCart").style.color = "white";
+//   document.getElementById("addToCart").textContent = "Ajouter au panier";
+//   console.log(productColor);
+// });
+
+// let quantityChoice = document.querySelector('input[id="quantity"]');
+// // On écoute ce qu'il se passe dans input[name="itemQuantity"]
+// quantityChoice.addEventListener("input", (eq) => {
+//   //reset
+//   document.getElementById("addToCart").style.color = "white";
+//   document.getElementById("addToCart").textContent = "Ajouter au panier";
+//   console.log(productQuantity);
+// });
+
+
+// // déclaration tableau qui sera le 1er, unique et destiné à initialiser le panier
+// let clientProductChoice = [];
+// let productSave = [];
+// let temporaryProduct = [];
+// let pushingProduct = [];
+
+// function addFirstProduct() {
+//   console.log(productSave);
+//   //if productSave est null, existe pas/ pas créé
+//   if (productSave === null) {
+//     // push le produit choisit
+//     clientProductChoice.push(productAddToCart);
+//     console.log(productAddToCart);
+//     return (localStorage.cartStock = JSON.stringify(clientProductChoice));
+//   }
+// }
+
+// function addOtherProduct() {
+//   // vide/initialise pushing... pour recevoir les nouvelles données
+//   pushingProduct = [];
+//   // push le produit choisit dans temporary
+//   temporaryProduct.push(productAddToCart);
+//   pushingProduct = productSave.concat(temporaryProduct);
+//   //Triage avec technologie "sort"
+//   pushingProduct.sort(function sorting(a, b) {
+//     if (a._id < b._id) return -1;
+//     if (a._id > b._id) return 1;
+//     if (a._id = b._id){
+//       if (a.color < b.color) return -1;
+//       if (a.color > b.color) return 1;
+//     }
+//     return 0;
+//   });
+//   temporaryProduct = [];
+//   return (localStorage.cartStock = JSON.stringify(pushingProduct));
+// }
+
+// function Cart() {
+//   productSave = JSON.parse(localStorage.getItem("cartStock"));
+//   if (productSave) {
+//     for (let choice of productSave) {
+//       //comparateur articles choisis et déjà ajoutés
+//       if (choice._id === productId && choice.color === productAddToCart.color) {
+//         alert("RAPPEL: Ce produit existe déjà dans votre panier");
+//         let addQuantity = parseInt(choice.quantity) + parseInt(productQuantity);
+//         choice.quantity = JSON.stringify(addQuantity);
+//         // dernière commande, on renvoit un nouveau panierStocké dans le localStorage
+//         return (localStorage.cartStock = JSON.stringify(productSave));
+//       }
+//     }
+//     return addOtherProduct();
+//   }
+//   return addFirstProduct();
+// }
